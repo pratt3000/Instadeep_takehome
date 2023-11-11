1b8a9dab24d84d7977364a940e112443a29e9b3a
pratt3000 <pratt3000@gmail.com>
2023-11-10 20:41:56 -0600

    feat: encoded model pred generated

--- src/predict.py
+++ src/predict.py
@@ -10,12 +10,9 @@ def get_argparse_arguments():
 
     # Add the arguments
     parser.add_argument('--input_seq', type=str, default="NA", help='Input protein sequence')
-    parser.add_argument('--checkpoint_dir', type=str, default="model_weights", help='Directory for saved checkpoints')
+    parser.add_argument('--model_checkpoint', type=str, default="lightning_logs/version_10/checkpoints/epoch=2-step=12738.ckpt", help='Directory for saved checkpoints')
+    parser.add_argument('--lang_params', type=str, default="lightning_logs/lang_params.pickle", help='Language params file')
     parser.add_argument('--gpu', action='store_true', help='Use GPU for training')
-    parser.add_argument('--num_gpus', type=int, default=0, help='number of gpus to use')
-    parser.add_argument('--cpu', action='store_true', help='Use CPU for training')
-    parser.add_argument('--max_seq_len', type=int, default=120,
-                        help='Max sequence length of protein (avoid changing this)')
 
     # Parse the arguments
     return parser.parse_args()
@@ -23,19 +20,30 @@ def get_argparse_arguments():
 
 if __name__ == "__main__":
 
+    # argument parser
+    args = get_argparse_arguments()
+
+    # Sets the device
+    if args.gpu and (torch.cuda.is_available() or torch.backends.mps.is_available()):
+        device = "cuda" if torch.cuda.is_available() else "mps"
+    else:
+        if args.gpu:
+            print("Warning: --gpu is set but no GPU is found on this machine. Using CPU instead.")
+        device = "cpu"
+    print(f"Device: {device}")
+
     # Load model weight [Easier way]
-    model = ProtCNN.load_from_checkpoint("lightning_logs/version_10/checkpoints/epoch=2-step=12738.ckpt")
+    model = ProtCNN.load_from_checkpoint(args.model_checkpoint).to(device)
 
     # Get language params for encoding input.
-    lang_params_file = "lightning_logs/lang_params.pickle"
-    with open(lang_params_file, 'rb') as handle:
+    with open(args.lang_params, 'rb') as handle:
         lang_params = pickle.load(handle)
 
     # Construct language encoder and encoder input
     lang_encoder = SequenceDataset(lang_params["word2id"], lang_params["fam2label"], lang_params["max_seq_len"], None, None)
-    x_encoded = lang_encoder.encode_single_sample("INPUT", y=None)
-    x_encoded = x_encoded[0].reshape((1, 22, 120)).to("mps") # formatting
+    x_encoded = lang_encoder.encode_single_sample(args.input_seq)
+    x_encoded = x_encoded[0].reshape((1, 22, 120)).to(device)
 
     pred = model(x_encoded)
-
+    print(pred)
 
